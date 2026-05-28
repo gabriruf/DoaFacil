273f6ca9e943f5261e263945f739c1ccb69fe160
gabriruf <grufinosouza@tutanota.com>
2026-05-28 14:33:46 -0300

    refactor(table-creation): melhorando a segurança ao criar a tabela de usuários + mudando o tipo de dado do campo "tipo_user"
    O método que executa a query que cria a tabela, caso ela não exista, foi trocado por uma alternativa mais segura contra ataques de "SQL Injection". A outra mudança esta no tipo de dado do campo "tipo_user", que deixará de ser "NUMERIC(1)" e passará a ser um "VARCHAR(9) NOT NULL", para melhor legibilidade de código pois agora, o tipo de acesso será explicitamente "Donatário" e "Doador" ao inves de 0 ou 1.
    
    BREAKING CHANGE: será necessário alterar o tipo de dado manualmente para usuários com a tabela já aplicada, e que estava seguindo a regra antiga.

--- php/tabelaUser.php
+++ php/tabelaUser.php
@@ -1,21 +1,22 @@
 <?php
-try {
-    $tables = $conn -> query("SHOW TABLES LIKE 'Users'");
-    if ($tables->rowCount() < 1) {
-        $conn -> query(
-            "CREATE TABLE doafacil.Users (
-                id_user       SERIAL PRIMARY KEY,
-                nome_user     VARCHAR(50),
-                email_user    VARCHAR(70),
-                endereco_user VARCHAR(70),
-                cel_user      VARCHAR(14),
-                cep_user      VARCHAR(9),
-                tipo_user     NUMERIC(1),
-                pass_user     VARCHAR(255)
-            );"
-        );
+    try {
+        $tables = $conn->prepare("SHOW TABLES LIKE 'Users'");
+        $tables->execute();
+        if ($tables->rowCount() < 1) {
+            $conn->prepare(
+                "CREATE TABLE doafacil.Users (
+                    id_user       SERIAL PRIMARY KEY,
+                    nome_user     VARCHAR(50) NOT NULL,
+                    email_user    VARCHAR(70) NOT NULL,
+                    endereco_user VARCHAR(70),
+                    cel_user      VARCHAR(14),
+                    cep_user      VARCHAR(9),
+                    tipo_user     VARCHAR(9) NOT NULL,
+                    pass_user     VARCHAR(255) NOT NULL
+                );"
+            )->execute();
+        }
+    } catch(PDOException $e) {
+        // Handle errors during db creation
+        echo "Erro ao criar tabela: $sql <br>" . $e->getMessage();
     }
-} catch(PDOException $e) {
-    // Handle errors during db creation
-    echo "Error creating table: " . $sql . "<br>" . $e->getMessage();
-}
